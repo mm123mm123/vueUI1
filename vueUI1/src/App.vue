d4bd70c8dfedff87da7d5a425eb9a30f03d7c100
mimi <fiymi666@gmail.com>
2021-01-14 11:18:51 +0800

    初始化项目

---
+++ vueUI1/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  app组件
+  <Frank/>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
